27d68b95abba20fa6457081429ca5361a9dd19fe
daukadolt <damirkhanov01@gmail.com>
2020-08-21 20:20:13 +0600

    UserController -> UsersController для нотации
    Signed-off-by: daukadolt <damirkhanov01@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import UserController from './controllers/UserController';
+import UserController from './controllers/UsersController';
 import AuthController from './controllers/AuthController';
 
 const app = express();
